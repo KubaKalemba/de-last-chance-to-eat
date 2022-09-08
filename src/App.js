a9d0078d4af0c26f5525dc83a432f061438c4df6
Kuba <kuba.kalemba@gmail.com>
2022-09-08 22:12:32 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NewIngredient from "./components/new-ingredient/NewIngredient";
 
 function App() {
   return (
     <div className="App">
       kkk
+      <NewIngredient />
     </div>
   );
 }
