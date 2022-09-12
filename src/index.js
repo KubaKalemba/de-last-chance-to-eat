92fd927a7915fe899e043932980340e57386ff2a
Kuba <kuba.kalemba@gmail.com>
2022-09-12 13:27:44 +0200

    test

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import {BrowserRouter} from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 
-/*const client = new ApolloClient({
+const client = new ApolloClient({
     uri: "https://rs-food-craft.herokuapp.com/graphql",
     cache: new InMemoryCache(),
-});*/
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    /*<ApolloProvider client={client}>*/
+    <ApolloProvider client={client}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
- /*   </ApolloProvider>*/
+    </ApolloProvider>
 );
